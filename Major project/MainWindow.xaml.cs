af6ab2cc063fe0f933bf0378388ded184507bdb3
Nicholas Wylynko <1014375@student.ccgs.wa.edu.au>
2019-07-30 11:24:27 +0800

    small change

--- Major project/MainWindow.xaml.cs
+++ Major project/MainWindow.xaml.cs
@@ -1,24 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Major_project
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
 
@@ -27,21 +15,76 @@ namespace Major_project
         public MainWindow()
         {
             InitializeComponent();
-            Console.WriteLine(Backend.getMessages(1));
+        }
+
+        public static void addMessageToChat(String msg)
+        {
+            ///chat.Items.Add(msg);
+            Console.WriteLine(msg);
         }
     }
 
     public class BackendConnect
     {
+        static String ip = "127.0.0.1";
+        static String port = "3000";
+        static String protocol = "http";
+        static String server = protocol + "://" + ip + ":" + port + "/";
+        String responseString;
 
-        public String server_ip = "http://127.0.0.1:3000/";
         static HttpClient client = new HttpClient();
 
-        static async void getMessages(int id)
+        private async void get(String request)
         {
-            String request = new String(server_ip + "getmessages/" + id);
-            var responseString = await client.GetStringAsync(request);
+            responseString = await client.GetStringAsync(request);
+            //client.AsyncWaitHandle.WaitOne();   
+        }
+
+        private async void post(String request, Dictionary<string, string> post_data)
+        {
+            //var values = new Dictionary<string, string>
+            //{
+            //   { "thing1", "hello" },
+            //   { "thing2", "world" }
+            //};
+
+            var content = new FormUrlEncodedContent(post_data);
+            var response = await client.PostAsync(request, content);
+            var responseString = await response.Content.ReadAsStringAsync();
             Console.WriteLine(responseString);
+        }
+
+        public String return_get(String request)
+        {
+            get(request);
+            return responseString;
+        }
+
+        public String return_post(String request, Dictionary<string, string> post_data)
+        {
+            post(request, post_data);
+            return responseString;
+        }
+
+        public void send_message()
+        {
+            String request = server + "message/1";
+
+            var post_data = new Dictionary<string, string>
+            {
+               { "thing1", "hello" },
+               { "thing2", "world" }
+            };
+
+            post(request, post_data);
+        }
+
+        public String get_messages(int id)
+        {
+            String request = server + "server/messages/" + id.ToString();
+            return_get(request);
+            Console.WriteLine("yonk: " + responseString);
+            return responseString;
         }
     }
 }
