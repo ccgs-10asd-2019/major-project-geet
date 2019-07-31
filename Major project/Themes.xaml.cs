6ee480292d4ffb571443b65d9871309149e631d0
Wenhan Li <1093347@student.ccgs.wa.edu.au>
2019-07-31 11:47:40 +0800

    background change

--- Major project/Themes.xaml.cs
+++ Major project/Themes.xaml.cs
@@ -28,16 +28,31 @@ namespace Major_project
         {
             var Scheme1 = "#FF9900";
             var Scheme2 = "#3DD3E9";
-            Change_theme(Scheme1, Scheme2);
+            Change_colour(Scheme1, Scheme2);
         }
 
-        public void Change_theme(string Scheme1, string Scheme2)
+
+
+        public void Change_colour(string Scheme1, string Scheme2)
         {
             var converter = new BrushConverter();
             var brush1 = (Brush)converter.ConvertFromString(Scheme1);
             var brush2 = (Brush)converter.ConvertFromString(Scheme2);
             Rectangle1.Fill = brush1;
             header_block.Fill = brush2;
+        }
+
+        private void Background1_clicked(object sender, RoutedEventArgs e)
+        {
+            string Background = "images/background.png";
+            Change_background(Background);
+        }
+
+        public void Change_background(string Background)
+        {
+            var converter1 = new BrushConverter();
+            var Background_set = (ImageBrush)converter1.ConvertFromString(Background);
+            ImageBrush ImageSource = Background_set;
         }
     }
 }
