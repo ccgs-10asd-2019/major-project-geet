085ee26d09223294de01a9dc1a6150758a457e17
Nicholas Wylynko <1014375@student.ccgs.wa.edu.au>
2019-08-05 08:35:36 +0800

    removed broken things

--- node/app.js
+++ node/app.js
@@ -7,13 +7,10 @@ app.use(express.json()); //adds json functionality
 const sqlite3 = require('sqlite3') //adds ability to manipulate sqlite db files
 const tools = require('./components/tools')
 
-//vars
-var db_fail = "";
-
 //setup
 console.log("Time: " + new Date()) //log to console time of server start
 db = { //connect to db files
-    "main": tools.connectToDB('main'), //stores core lists, servers, users
+    "main": tools.connectToDB('emain'), //stores core lists, servers, users
     "chat": tools.connectToDB('chats'), //stores all chat messages
     "chat_users": tools.connectToDB('chat_users'), //stores users allowed to message in a chat
     "users_chats": tools.connectToDB('users_chats'), //stores all the chats a user is in
@@ -21,14 +18,7 @@ db = { //connect to db files
 
 //gets and posts
 app.get('/', (req, res) => {
-    if(db_fail != "") { //if an error is being stored in db_fail because there was an error opening a db file
-        res.write("can't connect to database(s)\n")
-        res.write(db_fail) // error code
-        res.end()
-    }
-    else {
-        res.send("all g") //all is good
-    }
+    res.send("all g") //all is good
 })
 
 require('./components/messages')(app);
