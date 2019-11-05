88470072aaaebdb2573656eb18cc648909ac67f0
Wenhan Li <1093347@student.ccgs.wa.edu.au>
2019-11-05 09:47:46 +0800

    addchat Ui thing

---
+++ Major project/AddChat.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Major_project
+{
+    /// <summary>
+    /// Interaction logic for AddChat.xaml
+    /// </summary>
+    public partial class AddChat : Window
+    {
+        public AddChat()
+        {
+            InitializeComponent();
+            StartUpAddChat();
+        }
+
+        private void StartUpAddChat()
+        {
+            var imgBrush = new ImageBrush();
+            imgBrush.ImageSource = new BitmapImage(new Uri(Properties.Settings.Default.BackgroundUrl));
+            BackgroundGrid.Background = imgBrush;
+        }
+    }
+}
